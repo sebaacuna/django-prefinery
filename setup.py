be3eb444e7cadebf5e96270a8db0ac8ce720d3fd
Sebastian Acuna <sacuna@gmail.com>
2012-07-26 20:16:39 -0400

    Added templates to package_data

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from setuptools import setup, find_packages
 setup(name='django-prefinery',
       version='0.1',
       packages=find_packages(exclude=[]),
+      package_data={'prefinery': ['templates/prefinery/*']},
       exclude_package_data={'prefinery': ['bin/*.pyc']},
       )
