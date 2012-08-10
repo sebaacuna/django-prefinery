dae478d93b39799e3497828c214afa789313f7e6
Sebastian Acuna <sacuna@gmail.com>
2012-08-10 18:12:59 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-prefinery',
-      version='0.1',
+      version='0.1.1',
       packages=find_packages(exclude=[]),
       package_data={'prefinery': ['templates/prefinery/*']},
       exclude_package_data={'prefinery': ['bin/*.pyc']},
